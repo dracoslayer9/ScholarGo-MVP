0c27eca281811a3d22bb4a65f6a14980e9f75058
dracoslayer9 <dracoslayer81@gmail.com>
2025-12-17 16:43:17 +0700

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'paper': '#FDFBF7',
+        'oxford-blue': '#0F172A',
+        'bronze': '#B45309',
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['"Inter"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
